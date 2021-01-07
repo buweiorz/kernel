e8f6305879219ab15e2a945fe84fb42643de0a2e
Borislav Petkov <bp@suse.de>
2021-01-07 13:29:05 +0100

    thermal: Move therm_throt there from x86/mce
    commit 9223d0dccb8f8523754122f68316dd1a4f39f7f8 upstream.
    
    This functionality has nothing to do with MCE, move it to the thermal
    framework and untangle it from MCE.
    
    Intel_SIG: commit 9223d0dccb8f thermal: Move therm_throt there from
    x86/mce.
    Backport for Intel HFI (Hardware Feedback Interface) support
    
    Requested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Tested-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Link: https://lkml.kernel.org/r/20210202121003.GD18075@zn.tnic
    [ yingbao jia: amend commit log ]
    Signed-off-by: yingbao jia <yingbao.jia@intel.com>
    Signed-off-by: Yunhui Cui <cuiyunhui@bytedance.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -25,6 +25,7 @@
 #include <asm/traps.h>
 #include <asm/resctrl.h>
 #include <asm/numa.h>
+#include <asm/thermal.h>
 
 #ifdef CONFIG_X86_64
 #include <linux/topology.h>
@@ -723,6 +724,8 @@ static void init_intel(struct cpuinfo_x86 *c)
 
 	split_lock_init();
 	bus_lock_init();
+
+	intel_init_thermal(c);
 }
 
 #ifdef CONFIG_X86_32
