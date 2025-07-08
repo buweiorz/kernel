4f3720b3e1990503eecfdffc01d150408db51175
chenyi <chenyi211@h-partners.com>
2025-07-08 10:12:27 +0800

    Revert "drm: Remove drm_mode_config::fb_base"
    This reverts commit 584e4f84a479a7c2fbe6c33e7bdcf2ce25d9d319.

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
@@ -37,6 +37,9 @@ struct hibmc_vdac {
 struct hibmc_drm_private {
 	/* hw */
 	void __iomem   *mmio;
+	void __iomem   *fb_map;
+	resource_size_t  fb_base;
+	resource_size_t  fb_size;
 
 	/* drm */
 	struct drm_device dev;
